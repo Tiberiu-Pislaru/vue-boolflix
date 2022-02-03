6b973cfa59251636d998a68e7eebdfe9c1316ead
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2022-02-03 13:11:28 +0100

    Creata la Search Box, senza il tag input. Fatta la chiamata API con una query statica

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <div id="app">
-   
+    <search-box @search='getMovies'/>
+    <main-container/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import SearchBox from './components/SearcheBox.vue'
+import MainContainer from './components/MainContainer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchBox,
+    MainContainer,
+  },
+  methods: {
+    getMovies(){
+      axios.get('https://api.themoviedb.org/3/search/movie?query=ritorno&api_key=f760cceed080dd564aa1a8619b8ac208').then((result)=>{
+        this.moviesList=result.data.results;
+        console.log(this.moviesList)
+      })
+    }
+  },
+  data(){
+    return {
+      moviesList:[]
+    }
   }
 }
 </script>
---
+++ src/components/SearcheBox.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button @click="$emit('search')">cliccami</button>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
